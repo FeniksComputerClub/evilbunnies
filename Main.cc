7a6696aa36e10eb3ece49ccea6d82506cccac254
David <none@gmail.com>
2017-04-10 13:34:03 +0200

    Add Helloworld tut for gtkmm

---
+++ Main.cc
@@ -0,0 +1,10 @@
+#include "Helloworld.h"
+#include <gtkmm/application.h>
+
+int main (int argc, char *argv[])
+{
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+      HelloWorld helloworld;
+        //Shows the window and returns when it is closed.
+	  return app->run(helloworld);
+}

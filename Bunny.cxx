99e09b1f758fcfcc24f58a1c03101f46f780b373
David <none@gmail.com>
2017-05-29 14:52:01 +0200

    ai-statefultask add

--- Bunny.cxx
+++ Bunny.cxx
@@ -6,17 +6,12 @@
     #define M_PI 3.14159265358979323846
 #endif
 
-bool Bunny::on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& time)
+bool Bunny::on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& /*time*/)
 {
-  static double color = 0.0;
-  for (int i = 0; i < 10; ++i)
-  {
-    // Red circle.
-    cr->set_source_rgb(1.0, color, color);
-    cr->arc(m_x + i * 0.04, m_y, radius, 0.0, 2 * M_PI);
+  
+    // bunny
+    //cr->set_source_rgb(1.0, color, color);
+    cr->arc(m_x, m_y,m_radius, 0.0, 2 * M_PI);
     cr->fill_preserve();
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
-  color += 1.0;
   return true;
 }

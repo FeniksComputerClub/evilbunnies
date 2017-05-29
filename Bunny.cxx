a157c3a3bda2af0d048c741090aea0569f3dfea8
Carlo <feniks.carlo@gmail.com>
2017-05-29 14:09:02 +0200

    Use m_pattern.

--- Bunny.cxx
+++ Bunny.cxx
@@ -6,11 +6,10 @@
     #define M_PI 3.14159265358979323846
 #endif
 
-bool Bunny::on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& time)
+bool Bunny::on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& /*time*/)
 {
-  static double color = 0.0;
   // Red circle.
-  cr->set_source_rgb(1.0, color, color);
+  cr->set_source(m_pattern);
   cr->arc(m_x + m_radius, m_y + m_radius, m_radius, 0.0, 2 * M_PI);
   cr->fill_preserve();
   return true;

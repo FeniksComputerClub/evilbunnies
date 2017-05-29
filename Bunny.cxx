c74429d4bfe5eb7efb683e2fdff27f919d848d12
Carlo <feniks.carlo@gmail.com>
2017-05-29 14:42:39 +0200

    Stuff here

--- Bunny.cxx
+++ Bunny.cxx
@@ -10,7 +10,7 @@ bool Bunny::on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& /
 {
   // Red circle.
   cr->set_source(m_pattern);
-  cr->arc(m_x + m_radius, m_y + m_radius, m_radius, 0.0, 2 * M_PI);
+  cr->arc(m_x, m_y, m_radius, 0.0, 2 * M_PI);
   cr->fill_preserve();
   return true;
 }
--- MyArea.cxx
+++ MyArea.cxx
@@ -19,7 +19,8 @@ bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
   cr->save();
 
   // Move coordinate system to the center of the window.
-  cr->scale(width, height);
+  cr->scale(width, -height);
+  cr->translate(0.0, -1.0);
 
   bool handled = m_bunny.on_draw(cr, clock_type::now());
 

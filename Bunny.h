8f63e7f5a6a2b18ce2577eef56b234580430f172
Carlo <feniks.carlo@gmail.com>
2017-05-29 14:08:08 +0200

    Use Cairo::Pattern

--- Bunny.h
+++ Bunny.h
@@ -7,8 +7,10 @@ class Bunny {
   double m_radius;
   double m_x;
   double m_y;
+  Cairo::RefPtr<Cairo::Pattern> m_pattern;
+
   public:
-  Bunny() : m_radius(0.02), m_x(0.0), m_y(0.0) { }
+  Bunny() : m_radius(0.02), m_x(0.0), m_y(0.0), m_pattern(Cairo::SolidPattern::create_rgb(1.0, 0.0, 0.0)) { }
 
   bool on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& time);
 };

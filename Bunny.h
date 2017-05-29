24fb97401038ada85eee9d239ca88321138a16bf
Carlo <feniks.carlo@gmail.com>
2017-05-29 13:45:11 +0200

    Make radius variable.

--- Bunny.h
+++ Bunny.h
@@ -1,12 +1,14 @@
 #include <gtkmm/drawingarea.h>
 #include "clock.h"
 
+// Bunnies live in a pen of size 1.0 by 1.0,
+// with the origin in the bottom-left.
 class Bunny {
-  static constexpr double radius = 0.02;
+  double m_radius;
   double m_x;
   double m_y;
   public:
-  Bunny() : m_x(0.0), m_y(0.0) { }
+  Bunny() : m_radius(0.02), m_x(0.0), m_y(0.0) { }
 
   bool on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& time);
 };
--- MyArea.cxx
+++ MyArea.cxx
@@ -20,7 +20,6 @@ bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
 
   // Move coordinate system to the center of the window.
   cr->scale(width, height);
-  cr->translate(0.5, 0.5);
 
   bool handled = m_bunny.on_draw(cr, clock_type::now());
 

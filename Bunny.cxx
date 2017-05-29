2eed65af6844626346c7b0583d49485861bd4c25
TibbeFeniks <TibbeFeniks@users.noreply.github.com>
2017-05-29 14:45:10 +0200

    move bunny

--- Bunny.cxx
+++ Bunny.cxx
@@ -8,9 +8,11 @@
 
 bool Bunny::on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& /*time*/)
 {
-  // Red circle.
   cr->set_source(m_pattern);
   cr->arc(m_x + m_radius, m_y + m_radius, m_radius, 0.0, 2 * M_PI);
   cr->fill_preserve();
+  
+  //move bunny
+  m_x += 0.01;
   return true;
 }

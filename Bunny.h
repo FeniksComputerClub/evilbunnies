9aa89f5c2112b22382736b3f0f06b3ad4ead82f8
TibbeFeniks <TibbeFeniks@users.noreply.github.com>
2017-05-30 09:55:40 +0200

    implement statefultask

--- Bunny.h
+++ Bunny.h
@@ -1,14 +1,20 @@
 #include <gtkmm/drawingarea.h>
 #include "clock.h"
+#include "statefultask/AIStatefulTask.h"
 #include <algorithm>
 
 // Bunnies live in a pen of size 1.0 by 1.0,
 // with the origin in the bottom-left.
-class Bunny {
+class Bunny : public AIStatefulTask {
   double m_radius;
   double m_x;
   double m_y;
   Cairo::RefPtr<Cairo::Pattern> m_pattern;
+  
+  enum design_state_type {
+    Task_start = direct_base_type::max_state,
+    Task_done,
+  };
 
   public:
   Bunny() : m_radius(0.02), m_x(0.0), m_y(0.0), m_pattern(Cairo::SolidPattern::create_rgb(1.0, 0.0, 0.0))
@@ -24,3 +30,31 @@ class Bunny {
 
   bool on_draw(Cairo::RefPtr<Cairo::Context> const& cr, time_point const& time);
 };
+
+char const* Task::state_str_impl(state_type run_state) const
+{
+  switch(run_state)
+  {
+    AI_CASE_RETURN(Task_start);
+    AI_CASE_RETURN(Task_done);
+  }
+  return "UNKNOWN";
+}
+
+void Task::multiplex_impl(state_type run_state)
+{
+  switch(run_state)
+  {
+    case Task_start:
+      if (!m_do_finish)
+      {
+        wait(1);
+        break;
+      }
+      set_state(Task_done);
+      /*fall-through*/
+    case Task_done:
+      finish();
+      break;
+  }
+}

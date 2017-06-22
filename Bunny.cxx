1b5fdcf43803aaf759f3de00684bd24396b7b633
TibbeFeniks <TibbeFeniks@users.noreply.github.com>
2017-06-22 10:10:38 +0200

    randomize starting y

--- Bunny.cxx
+++ Bunny.cxx
@@ -40,7 +40,8 @@ void Bunny::multiplex_impl(state_type run_state)
   switch(run_state)
   {
     case start:
-      pos_w->addx((rand() % 100 + 1) / 100.0);
+      pos_w->setx((rand() % 100 + 1) / 100.0);
+      pos_w->sety((rand() % 100 + 1) / 100.0);
       set_state(change);
       //fallthrough
     case change:
